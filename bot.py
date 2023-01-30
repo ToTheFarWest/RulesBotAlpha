75527863c422cf724445108e0a3531ab0d1a3fa2
ToTheFarWest <yardenakin@gmail.com>
2023-01-30 12:44:30 +0200

    Moved proxy to commit, updated gitignore

--- bot.py
+++ bot.py
@@ -9,16 +9,12 @@ import requests
 from config import config
 
 TOKEN = config["token"]
-PROXIES = {
-    'http': 'http://10.0.0.10:80',
-    'https': 'http://10.0.0.10:80',
-}
 QUESTION_ID = 0
 QUESTION = 1
 SENTENCE_ID = 2
 SENTENCE = 3
 LABEL = 4
-RULES_FILE = R"C:\Users\u2225622\Desktop\WikiQACorpus\AlphaRules.csv"
+RULES_FILE = config["rules"]
 
 
 class BotHandler:
@@ -30,20 +26,20 @@ class BotHandler:
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params, proxies=PROXIES)
+        resp = requests.get(self.api_url + method, params)
         result_json = resp.json()['result']
         return result_json
 
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
         method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params, proxies=PROXIES)
+        resp = requests.post(self.api_url + method, params)
         return resp
 
     def send_photo(self, chat_id, photo):
         params = {'chat_id': chat_id, 'photo': photo}
         method = 'sendPhoto'
-        resp = requests.post(self.api_url + method, params, proxies=PROXIES)
+        resp = requests.post(self.api_url + method, params)
         return resp
 
     def get_last_update(self):
